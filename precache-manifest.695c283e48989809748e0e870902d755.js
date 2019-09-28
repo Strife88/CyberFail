c73111d8c2d8a2919a15c944472c8b899746e6a6
Krasnyi Danil <dan2000kr@gmail.com>
2019-09-28 18:29:25 +0300

    hackaton competition

---
+++ precache-manifest.695c283e48989809748e0e870902d755.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35d44b62b97683e4f008455873b89b06",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4f92e4a9e83f02604931",
+    "url": "./static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "4f92e4a9e83f02604931",
+    "url": "./static/js/2.29a449f4.chunk.js"
+  },
+  {
+    "revision": "8c2c570c16334ef130a8",
+    "url": "./static/js/main.91d6b061.chunk.js"
+  },
+  {
+    "revision": "117eee4a239403c669e0",
+    "url": "./static/js/runtime-main.b9f966ec.js"
+  },
+  {
+    "revision": "7f737304b18404f79d0d8089d90cb99e",
+    "url": "./static/media/empty.7f737304.svg"
+  }
+]);
